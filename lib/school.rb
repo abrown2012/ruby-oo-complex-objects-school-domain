f01a623f90c4112698931c2f7563c16726758087
Agnieszka Brown <abrown2012@icloud.com>
2020-11-04 21:23:52 -0500

    commit tree 0ad4d7ff9c9d90aaed05794b490721bfb7b46fb0

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School    
+    attr_accessor :roster
+    def initialize(name)
+        @name = name         
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        if @roster[grade]
+            @roster[grade] << student_name 
+        else 
+            @roster[grade] = []
+            @roster[grade] << student_name
+        end 
+    end 
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |grade, value|
+            @roster[grade] = value.sort
+        end
+    end
+
+
+end
